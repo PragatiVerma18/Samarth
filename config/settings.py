774ecc4988f67b28c141da0fc9858d16cab665c1
anubhav.s <anubhav.s@coinswitch.co>
2022-11-05 16:35:37 +0530

    user-auth added

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,31 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'django.contrib.sites',  
+
+    # 3rd-party apps
+    'rest_framework',
+    'rest_framework.authtoken',
+    'allauth',  
+    'allauth.account',  
+    'allauth.socialaccount',  
+
+    'dj_rest_auth.registration', 
+    'dj_rest_auth',
 ]
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' # new
+SITE_ID = 1 # new
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [ 
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication'
+    ],
+}
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
