880e6d554de89f835f7666e6827b17d48d4d1524
anubhav.s <anubhav.s@coinswitch.co>
2022-11-06 03:52:00 +0530

    added logo bg for LSPs

--- loan/constants.py
+++ loan/constants.py
@@ -5,24 +5,28 @@ loans = [
     {
         "uuid": str(uuid.uuid4()),
         "name": "ICICI VALUE-X01",
+        "logo": "https://dailynewspk.com/wp-content/uploads/2020/12/ICICI-Bank-1024x640.jpg",
         "value": "40000",
         "due_date": str(date.today() + timedelta(days=10))
     },
     {
         "uuid": str(uuid.uuid4()),
         "name": "HDFC MSME-R45",
+        "logo": "https://imgs.search.brave.com/S7Hb1R33jYdHnjvFVuPmbYJqBmKyyT-iLcYwNHEyvLI/rs:fit:640:480:1/g:ce/aHR0cHM6Ly93d3cu/aGluZHVzdGFudGlt/ZXMuY29tL3JmL2lt/YWdlX3NpemVfNjQw/eDM2Mi9IVC9wMi8y/MDE2LzA0LzIyL1Bp/Y3R1cmVzL2hkZmMt/YmFuay1sb2dvLW9m/aWNpYWwtd2ViaXN0/ZV85ZWJlZmI0Ni0w/OGI2LTExZTYtYWZj/ZS1jZDU5MDg3NGM2/N2UuanBn",
         "value": "24000",
         "due_date": str(date.today() + timedelta(days=14))
     },
     {
         "uuid": str(uuid.uuid4()),
         "name": "IDFC FIRST T50",
+        "logo": "https://imgs.search.brave.com/1Rwm5mn34jRwJsrBYmAEb2SV6pc54pDUrt5Xjk5vX1k/rs:fit:1200:900:1/g:ce/aHR0cHM6Ly9jZG4u/c2lhc2F0LmNvbS93/cC1jb250ZW50L3Vw/bG9hZHMvMjAyMC8w/NS9JREZDLUZpcnN0/LUJhbmsuanBn",
         "value": "37499",
         "due_date": str(date.today() + timedelta(days=21))
     },
     {
         "uuid": str(uuid.uuid4()),
         "name": "HDFC MIL T23",
+        "logo": "https://imgs.search.brave.com/S7Hb1R33jYdHnjvFVuPmbYJqBmKyyT-iLcYwNHEyvLI/rs:fit:640:480:1/g:ce/aHR0cHM6Ly93d3cu/aGluZHVzdGFudGlt/ZXMuY29tL3JmL2lt/YWdlX3NpemVfNjQw/eDM2Mi9IVC9wMi8y/MDE2LzA0LzIyL1Bp/Y3R1cmVzL2hkZmMt/YmFuay1sb2dvLW9m/aWNpYWwtd2ViaXN0/ZV85ZWJlZmI0Ni0w/OGI2LTExZTYtYWZj/ZS1jZDU5MDg3NGM2/N2UuanBn",
         "value": "10999",
         "due_date": str(date.today() + timedelta(days=30))
     },
@@ -30,17 +34,20 @@ loans = [
         "uuid": str(uuid.uuid4()),
         "name": "ICICI VALUE-X01",
         "value": "40000",
+        "logo": "https://dailynewspk.com/wp-content/uploads/2020/12/ICICI-Bank-1024x640.jpg"
         "due_date": str(date.today() + timedelta(days=10))
     },
     {
         "uuid": str(uuid.uuid4()),
         "name": "PNB PRO MSME-R45",
+        "logo": "https://imgs.search.brave.com/Eac_vpCKdG2P3RwfvDu-dbHetDIaGgyQL2QxJVkRfoU/rs:fit:1200:720:1/g:ce/aHR0cHM6Ly9jZG4u/ZG5haW5kaWEuY29t/L3NpdGVzL2RlZmF1/bHQvZmlsZXMvc3R5/bGVzL2Z1bGwvcHVi/bGljLzIwMTYvMDEv/MDIvNDEwNTc5LXBu/Yi5qcGc",
         "value": "24000",
         "due_date": str(date.today() + timedelta(days=14))
     },
     {
         "uuid": str(uuid.uuid4()),
         "name": "SBI SAFAR T50",
+        "logo": "https://imgs.search.brave.com/TBL07c8jDrzGB4psvma01d9pMo3kbZqXJWxo46bue1k/rs:fit:1149:591:1/g:ce/aHR0cDovL3BuZ2lt/YWdlc2ZyZWUuY29t/L0xPR08vUy9zYmkt/YmFuay9TQkktTG9n/by1WRUNUT1ItQ0RS/LnBuZw",
         "value": "37499",
         "due_date": str(date.today() + timedelta(days=21))
     },
@@ -48,6 +55,7 @@ loans = [
         "uuid": str(uuid.uuid4()),
         "name": "IDFC T23",
         "value": "10999",
+        "logo": "https://imgs.search.brave.com/1Rwm5mn34jRwJsrBYmAEb2SV6pc54pDUrt5Xjk5vX1k/rs:fit:1200:900:1/g:ce/aHR0cHM6Ly9jZG4u/c2lhc2F0LmNvbS93/cC1jb250ZW50L3Vw/bG9hZHMvMjAyMC8w/NS9JREZDLUZpcnN0/LUJhbmsuanBn",
         "due_date": str(date.today() + timedelta(days=30))
     }
 ]
