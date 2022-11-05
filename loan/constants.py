b43ea35a683b3005c8665dd42bf87c02e56b3777
anubhav.s <anubhav.s@coinswitch.co>
2022-11-06 04:15:48 +0530

    static files configured for heroku

--- loan/constants.py
+++ loan/constants.py
@@ -34,7 +34,7 @@ loans = [
         "uuid": str(uuid.uuid4()),
         "name": "ICICI VALUE-X01",
         "value": "40000",
-        "logo": "https://dailynewspk.com/wp-content/uploads/2020/12/ICICI-Bank-1024x640.jpg"
+        "logo": "https://dailynewspk.com/wp-content/uploads/2020/12/ICICI-Bank-1024x640.jpg",
         "due_date": str(date.today() + timedelta(days=10))
     },
     {
@@ -60,7 +60,7 @@ loans = [
     }
 ]
 
-txns =[
+txns = [
     {
         "uuid": str(uuid.uuid4()),
         "name": "Laxman Kirana Store",
@@ -68,7 +68,7 @@ txns =[
         "value": 18999,
         "type": "credit",
         "due_date": str(date.today() - timedelta(days=1)),
-        
+
     },
     {
         "uuid": str(uuid.uuid4()),
@@ -77,7 +77,7 @@ txns =[
         "value": 12500,
         "type": "credit",
         "due_date": str(date.today() - timedelta(days=2)),
-        
+
     },
     {
         "uuid": str(uuid.uuid4()),
@@ -86,7 +86,7 @@ txns =[
         "value": 25000,
         "type": "credit",
         "due_date": str(date.today() - timedelta(days=3)),
-        
+
     },
     {
         "uuid": str(uuid.uuid4()),
@@ -95,7 +95,7 @@ txns =[
         "value": 65000,
         "type": "debit",
         "due_date": str(date.today() - timedelta(days=4)),
-        
+
     },
     {
         "uuid": str(uuid.uuid4()),
@@ -104,7 +104,7 @@ txns =[
         "value": 71000,
         "type": "debit",
         "due_date": str(date.today() - timedelta(days=5)),
-        
+
     },
     {
         "uuid": str(uuid.uuid4()),
@@ -112,7 +112,7 @@ txns =[
         "gstn": "03***********A3",
         "value": 18999,
         "due_date": str(date.today() - timedelta(days=4)),
-        
+
     },
     {
         "uuid": str(uuid.uuid4()),
@@ -120,33 +120,33 @@ txns =[
         "gstn": "06***********A4",
         "value": 18999,
         "due_date": str(date.today() - timedelta(days=3)),
-        
+
     }
 ]
 
 
-offers =[
+offers = [
     {
         "id": 1,
         "name": "ICICI VALUE-X01",
         "interest": "6.5%",
         "ratings": "4/5"
-    },{
+    }, {
         "id": 2,
         "name": "HDFC MSME-R45",
         "interest": "7.1%",
         "ratings": "3.5/5"
-    },{
+    }, {
         "id": 3,
         "name": "IDFC FIRST T50",
         "interest": "7.3%",
         "ratings": "3/5"
-    },{
+    }, {
         "id": 4,
         "name": "HDFC MIL T23",
         "interest": "6.1%",
         "ratings": "4.2/5"
-    },{
+    }, {
         "id": 5,
         "name": "PNB PRO MSME-R45",
         "interest": "6%",
